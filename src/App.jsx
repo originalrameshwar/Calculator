51d8c298a8e7e56de5bf207bf4410f82a5c8672c
originalrameshwar <originalrameshwar@gmail.com>
2024-02-14 08:34:22 +0530

    fix Max width to 360px to avoid overflow and also set overflow-hidden

--- src/App.jsx
+++ src/App.jsx
@@ -9,67 +9,65 @@ function App() {
     try {
       console.log(val);
 
-    if (val === "AC") {
-      setInputValu("");
-      return;
-    } else if (val === "DE") {
-      setInputValu((prev) => prev.slice(0, prev.length - 1));
-      return;
-    } else if (val === "=") {
-  
-      const result = eval(inputValu);
-      if (result === undefined) {
-        return
-      }
-      // Check if the result is a floating-point number
-      const isFloatingPoint = result % 1 !== 0;
-
-      // If it's a floating-point number, round to four decimal places
-      setInputValu(
-        isFloatingPoint ? parseFloat(result).toFixed(4) : String(result)
-      );
-      return;
-    } else {
-      let result = inputValu + String(val);
-      console.log(result);
-
-      if (val === ".") {
-        if (inputValu === "") {
-          setInputValu("0.");
-          return;
-        } 
-      }
-
-      if (
-        result.charAt(result.length - 1) === "/" ||
-        result.charAt(result.length - 1) === "*" ||
-        result.charAt(result.length - 1) === "+" ||
-        result.charAt(result.length - 1) === "-"
-      ) {
-        if (
-          result.charAt(result.length - 2) === "/" ||
-          result.charAt(result.length - 2) === "*" ||
-          result.charAt(result.length - 2) === "+" ||
-          result.charAt(result.length - 2) === "-"
-        ) {
-          result = result.slice(0, result.length - 2);
-          setInputValu(result + val);
+      if (val === "AC") {
+        setInputValu("");
+        return;
+      } else if (val === "DE") {
+        setInputValu((prev) => prev.slice(0, prev.length - 1));
+        return;
+      } else if (val === "=") {
+        const result = eval(inputValu);
+        if (result === undefined) {
           return;
         }
-        setInputValu(result);
+        // Check if the result is a floating-point number
+        const isFloatingPoint = result % 1 !== 0;
+
+        // If it's a floating-point number, round to four decimal places
+        setInputValu(
+          isFloatingPoint ? parseFloat(result).toFixed(4) : String(result)
+        );
         return;
       } else {
-        setInputValu(result);
-        return;
+        let result = inputValu + String(val);
+        console.log(result);
+
+        if (val === ".") {
+          if (inputValu === "") {
+            setInputValu("0.");
+            return;
+          }
+        }
+
+        if (
+          result.charAt(result.length - 1) === "/" ||
+          result.charAt(result.length - 1) === "*" ||
+          result.charAt(result.length - 1) === "+" ||
+          result.charAt(result.length - 1) === "-"
+        ) {
+          if (
+            result.charAt(result.length - 2) === "/" ||
+            result.charAt(result.length - 2) === "*" ||
+            result.charAt(result.length - 2) === "+" ||
+            result.charAt(result.length - 2) === "-"
+          ) {
+            result = result.slice(0, result.length - 2);
+            setInputValu(result + val);
+            return;
+          }
+          setInputValu(result);
+          return;
+        } else {
+          setInputValu(result);
+          return;
+        }
       }
-    }
     } catch (error) {
       console.log(error);
       alert("Invalid input");
-      console.warn("Invalid input")
+      console.warn("Invalid input");
       setInputValu("");
-      return
-
+      return;
     }
   };
 
@@ -96,9 +94,7 @@ function App() {
 
   return (
     <>
-      {/* <h1>Hi everone</h1> */}
-
-      <div className="bg-[#3a4452] p-5 rounded-xl">
+      <div className="bg-[#3a4452] p-5 rounded-xl max-w-[360px] overflow-hidden">
         <div className="justify-end ">
           <div className="text-right shadow-none bg-transparent text-[40px] border-none min-h-16 max-w-[360px]  ">
             <span className="overflow-hidden">{inputValu}</span>
